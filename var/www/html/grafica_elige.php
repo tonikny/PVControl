553e3eea15d8322913f1ae098bf73d183fa54888
toni <toni@localhost>
2020-02-26 22:53:57 +0100

    Initial commit - imagen 2020-02-26

---
+++ var/www/html/grafica_elige.php
@@ -0,0 +1,272 @@
+<?php
+ require('conexion.php');
+ date_default_timezone_set("UTC");
+ $rango="ASEM";
+ $parametro="Produccion";
+
+ $sql = "SELECT FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7)) AS Fecha, TRUNCATE(SUM(Wh_placa/1000),2) as Datos
+                        FROM diario
+                        WHERE Fecha>= DATE_SUB(curdate(), INTERVAL (364 -2 + DAYOFWEEK(curdate())) DAY)
+                        GROUP BY FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7))
+                        ORDER BY FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7))";
+
+ $vartitulo = "Producción Anual kWh por Semanas";
+ $anchocol= 20;
+
+ if(( $_POST["parametro"] ) && ($_POST["rango"] )) {//|| (($rango=="MONTH") && ($parametro=="Produccion")){
+    $parametro = $_POST["parametro"];
+    $rango = $_POST["rango"];
+
+    if($parametro == "Produccion") {
+
+      $colorbar = 'rgba(100,149,237,0.8)';
+
+	  if($rango=="WEEK") {
+		$sql = "SELECT Fecha, TRUNCATE(Wh_placa/1000,2) as Datos FROM diario
+			WHERE WEEK(Fecha,1) = WEEK(CURDATE(),1) AND YEAR(Fecha) = YEAR(CURDATE())
+			GROUP BY Fecha";
+
+		$vartitulo = "Producción Semanal kWh";
+		$anchocol = 40;
+
+	  } elseif($rango=="MONTH") {
+		 $sql = "SELECT Fecha, TRUNCATE(Wh_placa/1000,2) as Datos FROM diario
+			WHERE MONTH(Fecha) = MONTH(CURDATE()) AND YEAR(Fecha) = YEAR(CURDATE())
+			GROUP BY Fecha";
+
+		 $vartitulo = "Producción Mensual kWh";
+		 $anchocol= 20;
+
+	  } elseif($rango=="ASEM") {
+
+         $sql = "SELECT FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7)) AS Fecha, TRUNCATE(SUM(Wh_placa/1000),2) as Datos
+                        FROM diario
+                        WHERE Fecha>= DATE_SUB(curdate(), INTERVAL (364 -2 + DAYOFWEEK(curdate())) DAY)
+                        GROUP BY FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7))
+                        ORDER BY FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7))";
+
+		 $vartitulo = "Producción Anual kWh por Semanas";
+		 $anchocol= 20;
+
+	  } else {
+
+          $sql = "SELECT STR_TO_DATE(concat('1,', MONTH(Fecha), ',', YEAR(Fecha)), '%d,%m,%Y') as Fecha, TRUNCATE(SUM(Wh_placa/1000),2) as Datos
+                        FROM diario
+                        WHERE Fecha >= STR_TO_DATE(concat('1,', MONTH(curdate()), ',', YEAR(curdate())-1), '%d,%m,%Y')
+                        GROUP BY YEAR(Fecha),MONTH(Fecha)";
+
+          $vartitulo = "Producción Anual kWh por Meses";
+          $anchocol= 40;
+
+	  }
+
+
+    } elseif($parametro == "Consumo") {
+
+	    $colorbar = 'rgba(255,0,0,0.8)';
+
+        if($rango=="WEEK") {
+                $sql = "SELECT Fecha, TRUNCATE((Wh_placa-(Whp_bat-kWhn_bat))/1000,2) as Datos FROM diario
+                        WHERE WEEK(Fecha,1) = WEEK(CURDATE(),1) AND YEAR(Fecha) = YEAR(CURDATE())
+                        GROUP BY Fecha";
+
+                $vartitulo = "Consumo kWh Semanal";
+                $anchocol = 40;
+
+        } elseif($rango=="MONTH") {
+                $sql = "SELECT Fecha, TRUNCATE((Wh_placa-(Whp_bat-Whn_bat))/1000,2) as Datos FROM diario
+                        WHERE MONTH(Fecha) = MONTH(CURDATE()) AND YEAR(Fecha) = YEAR(CURDATE())
+                        GROUP BY Fecha";
+
+                $vartitulo = "Consumo kWh Mensual";
+                $anchocol= 20;
+
+        } elseif($rango=="ASEM") {
+
+                $sql = "SELECT FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7)) AS Fecha, TRUNCATE(SUM(Wh_placa-(Whp_bat-Whn_bat))/1000,2) as Datos
+                        FROM diario
+                        WHERE Fecha>= DATE_SUB(curdate(), INTERVAL (364 -2 + DAYOFWEEK(curdate())) DAY)
+                        GROUP BY FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7))
+                        ORDER BY FROM_DAYS(TO_DAYS(Fecha) -MOD(TO_DAYS(Fecha) -2, 7))";
+
+
+                $vartitulo = "Consumo Anual kWh por Semanas";
+                $anchocol= 20;
+
+        } else {
+
+                $sql = "SELECT STR_TO_DATE(concat('1,', MONTH(Fecha), ',', YEAR(Fecha)), '%d,%m,%Y') as Fecha, TRUNCATE(SUM(Wh_placa-(Whp_bat-Whn_bat))/1000,2) as Datos
+                        FROM diario
+                        WHERE Fecha >= STR_TO_DATE(concat('1,', MONTH(curdate()), ',', YEAR(curdate())-1), '%d,%m,%Y')
+                        GROUP BY YEAR(Fecha),MONTH(Fecha)";
+
+                $vartitulo = "Consumo Anual kWh por Meses";
+                $anchocol= 40;
+
+        }
+
+    }
+}
+
+    if($result = mysqli_query($link, $sql)){
+
+        $total=0;
+	    $i=0;
+	    while($row1 = mysqli_fetch_assoc($result)) {
+		    //guardamos en rawdata todos los vectores/filas que nos devuelve la consulta
+		    $rawdata1[$i] = $row1;
+                    $total=$total+$rawdata1[$i]["Datos"];
+		    $i++;
+	    }
+
+    } else{
+
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    }
+
+
+    mysqli_close($link);
+
+
+    //Adaptar el tiempo grafica4
+    for($i=0;$i<count($rawdata1);$i++){
+        $time = $rawdata1[$i]["Fecha"];
+        $date = new DateTime($time);
+        $rawdata1[$i]["Fecha"]=$date->getTimestamp()*1000;
+    }
+
+
+?>
+
+<HTML>
+
+<body>
+
+<meta charset="utf-8">
+
+
+<script src="https://code.jquery.com/jquery.js"></script>
+
+<script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="http://code.highcharts.com/highcharts-more.js"></script>
+<script src="http://code.highcharts.com/themes/grid.js"></script>
+
+
+<form action = "<?php $_PHP_SELF ?>" method = "POST">
+    <select name="parametro">
+	<option value="Produccion">Producción</option>
+        <option value="Consumo">Consumo</option>
+    </select>
+    <select name="rango">
+        <option value="WEEK">Semana</option>
+        <option value="MONTH">Mes</option>
+	    <option value="ASEM" selected >AñoXSem</option>
+	    <option value="AMES">AñoXMes</option>
+    </select>
+    <input type = "submit" value = "Ver" />
+</form>
+
+<p></p>
+
+<div id="container1" style="width: auto;height: 60vh;margin-left: 5;margin-right:5"></div>
+<br>
+
+
+
+</body>
+
+<script>
+$(function () {
+
+	Highcharts.setOptions({
+            global: {
+                useUTC: true
+            },
+	    lang: {
+		months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+		weekdays: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+		shortMonths: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+	        rangeSelectorFrom: "Desde",
+	        rangeSelectorTo: "A",
+ 	        printChart: "Imprimir gráfico",
+		loading: "Cargando..."
+	    }
+        });
+
+        var chart1 = new Highcharts.Chart ({
+            chart: {
+                renderTo: 'container1',
+                zoomType: 'xy'
+            },
+
+            title: {
+                text: '<?php echo $vartitulo.' '.$total;?>'
+	    },
+
+            subtitle: {
+                //text: 'Permite Zoom XY'
+            },
+            credits: {
+                enabled: false
+            },
+	    xAxis: {
+                dateTimeLabelFormats: {
+                    day: '%e %b',
+                    week:'%e %b',
+                    month: '%b',
+                    year: '%Y'
+                },
+                type: 'datetime'    //datetime
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                labels: {
+                    enabled: true
+                }
+            },
+	    legend: {
+		enabled: false
+	    },
+            //tooltip: {
+                //valueSuffix: ' Wh',
+                //valueDecimals: 1
+            //},
+
+            plotOptions: {
+                column: {
+                    dataLabels: {
+                        enabled: true,
+                        formatter: function () {
+                            return Highcharts.numberFormat(this.y,1);
+                        },
+                        crop: false,
+                        overflow: 'none',
+                    },
+                    enableMouseTracking: false
+                }
+            },
+
+	    series: [{
+		name: 'Wh',
+		type: 'column',
+		pointWidth: '<?php echo $anchocol;?>', //Ancho fijado de las columnas
+		color: '<?php echo $colorbar;?>',
+		//color: Highcharts.getOptions().colors[5],
+		data: (function() {
+                   var data = [];
+                   <?php
+                       for($i = 0 ;$i<count($rawdata1);$i++){
+		   ?>
+		   data.push([<?php echo $rawdata1[$i]["Fecha"];?>,<?php echo $rawdata1[$i]["Datos"];?>]);
+                   <?php } ?>
+		return data;
+                     })()
+		}]
+	});
+});
+
+
+</script>
+</html>
---
+++ var/www/html/log.php
@@ -0,0 +1,101 @@
+<HTML>
+
+<body>
+
+<meta charset="utf-8">
+
+
+<form action = "<?php $_PHP_SELF ?>" method = "POST">
+    <select name="rango">
+        <option value="DAY" selected="selected">Hoy</option>
+        <option value="WEEK">Semana actual</option>
+        <option value="MONTH">Mes actual</option>
+    </select>
+    <input type = "submit" value = "Ver" />
+</form>
+<br>
+
+</body>
+</html>
+
+
+
+<?php
+
+
+require('conexion.php');
+
+if (!$_POST["rango"] ) {
+
+    $rango = "DAY";
+
+} else {
+
+    $rango=$_POST["rango"];
+}
+
+if($rango=="DAY") {
+		  echo "DIA";
+          $sql = "SELECT Tiempo,log FROM log
+                        WHERE DATE(Tiempo) = CURDATE()
+                        ORDER BY Tiempo DESC";
+
+} elseif($rango=="WEEK") {
+		  echo "SEMANA";
+          $sql = "SELECT Tiempo,log FROM log
+                        WHERE WEEK(Tiempo,1) = WEEK(CURDATE(),1) and log<>'Registro diario actualizado'
+                        ORDER BY Tiempo DESC";
+
+} elseif($rango=="MONTH") {
+	      echo "MES";
+          $sql = "SELECT Tiempo,log FROM log
+                        WHERE MONTH(Tiempo) = MONTH(CURDATE()) AND YEAR(Tiempo) = YEAR(CURDATE()) and log<>'Registro diario actualizado'
+                        ORDER BY Tiempo DESC";
+}
+
+
+if($result = mysqli_query($link, $sql)){
+
+	$rawdata=array();
+	$i=0;
+
+	while ($row = mysqli_fetch_array($result)){
+		$rawdata[$i]=$row;
+		$i++;
+	}
+
+	echo '<table width="56%" border="1" style="text-align:center;">';
+	$columnas = count($rawdata[0])/2;
+	$filas = count($rawdata);
+
+
+	//Añadimos los titulos
+
+	for($i=1;$i<count($rawdata[0]);$i=$i+2){
+		next($rawdata[0]);
+		echo "<th><b>".key($rawdata[0])."</b></th>";
+		next($rawdata[0]);
+	}
+
+	for($i=0;$i<$filas;$i++){
+
+		echo "<tr>";
+		for($j=0;$j<$columnas;$j++){
+			echo "<td>".$rawdata[$i][$j]."</td>";
+		}
+		echo "</tr>";
+	}
+
+	echo '</table>';
+
+} else{
+
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+mysqli_close($link);
+
+//echo "<br \>";
+
+?>
+
