c87c88fd71d930a3c85a22a0963505044435defa
mleon55555 <mleon55555@gmail.com>
2021-10-28 23:47:58 +0200

    Mejora para crear los enlaces de los servicios desde programa python3 No se necesita actualizar al incorporar nuevos servicios Procesa todos los archivos creados en carpeta .../PVControl+/etc/systemd/system

---
+++ Arrancar_servicios_PVControl+.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Versión 2021-10-28
+import os,glob
+import subprocess
+import colorama # colores en ventana Terminal
+from colorama import Fore, Back, Style
+colorama.init()
+
+carpeta= '/home/pi/PVControl+/etc/systemd/system/*.*'
+
+print(Fore.YELLOW+'######## Activando Sevicios #########')
+for f in glob.glob(carpeta):
+    if os.path.isfile(f):
+        print (Fore.RESET+'Procesando archivo.... '+ Fore.GREEN+f'{f}'+ Fore.RESET)
+        res = subprocess.run(['sudo','ln', '-s',f'{f}','/etc/systemd/system/{f[39:]}'], capture_output=True)
+        res = subprocess.run(['sudo','systemctl', 'enable', f'{f[39:]}'], capture_output=True)
+        res = subprocess.run(['sudo','systemctl', 'restart', f'{f[39:]}'], capture_output=True)
+        res = subprocess.run(['sudo','systemctl', 'status', f'{f[39:]}'], capture_output=True)
+        print (Fore.BLUE,res.stdout[-50:])   
+    else:
+        print(Fore.RED+f'{f} no es un fichero')
+    
+#Paginas web
+print()
+print(Fore.YELLOW+'######## Activando WEB PVControl+ #########')
+res = subprocess.run(['sudo','rm', '/var/www/html'])
+res = subprocess.run(['sudo','ln', '-s','/home/pi/PVControl+/html','/var/www'])
+print (Fore.GREEN+ '  ---- OK -----')
+#Crontab
+print()
+print(Fore.YELLOW+'######## Activando Procesos CRONTAB #########')
+res = subprocess.run(['sudo','chown', 'root','/home/pi/PVControl+/etc/cron.d/pvcontrol'])
+res = subprocess.run(['sudo','ln', '-s','/home/pi/PVControl+/etc/cron.d/pvcontrol','/etc/cron.d/pvcontrol'], capture_output=True)
+print (Fore.GREEN+ '  ---- OK -----')
+
+print()
+print(Fore.CYAN+'######## Proceso Completado #########')
