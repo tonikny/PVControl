039c0bd3d839120a1d363cfd06fd34aa4b872379
mleon55555 <mleon55555@gmail.com>
2021-12-27 16:59:30 +0100

    Actualizacion para incluir campos Wred etc

--- diario_offline.py
+++ diario_offline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Versión 2019-08-11
+# Versión 2021-12-27
 
 import MySQLdb
 import time 
@@ -23,16 +23,20 @@ def logBD() :
     return
 
 
-for i in range (12,16): #poner el rango de dias desde hoy para actualizar
+for i in range (1,11): #poner el rango de dias desde hoy para actualizar
     try:
 
         db = MySQLdb.connect(host = servidor, user = usuario, passwd = clave, db = basedatos)
         cursor = db.cursor()
 
-        sql="SELECT MAX(DATE(Tiempo)), MAX(Vbat), MIN(Vbat), AVG(Vbat), MAX(SOC), MIN(SOC), AVG(SOC), \
-                MAX(Ibat), MIN(Ibat), AVG(Ibat), MAX(Iplaca), AVG(Iplaca), MAX(kWh_placa), \
-                MAX(kWhp_bat), MAX(kWhn_bat), MAX(kWh_placa-(kWhp_bat-kWhn_bat)), MAX(Temp), MIN(Temp), AVG(Temp) \
-                FROM datos WHERE DATE(Tiempo) = SUBDATE(CURDATE(),"+str(i)+")"
+        sql = "SELECT MAX(DATE(Tiempo)), MAX(Vbat), MIN(Vbat), AVG(Vbat), MAX(SOC), MIN(SOC), AVG(SOC), \
+                MAX(Ibat), MIN(Ibat), AVG(Ibat), MAX(Iplaca), AVG(Iplaca), MAX(Wh_placa), \
+                MAX(Whp_bat), MAX(Whn_bat), MAX(Wh_placa-(Whp_bat-Whn_bat)-(Whp_red-Whn_red)), MAX(Temp), MIN(Temp), AVG(Temp), \
+                MAX(Whp_red), MAX(Whn_red), MAX(Wred), MIN(Wred),AVG(Wred),MAX(Vred), MIN(Vred) \
+                FROM datos WHERE DATE(Tiempo) = SUBDATE(CURDATE()," + str(i) + ")"
+        
+        #print (sql)
+        
         cursor.execute(sql)
         var=cursor.fetchall()
         
@@ -40,7 +44,8 @@ for i in range (12,16): #poner el rango de dias desde hoy para actualizar
 
         print (tiempo,"Error, la base de datos no existe")
 
-
+    
+    
     try:
         sql="SELECT DATE(Fecha) FROM diario WHERE DATE(Fecha) = SUBDATE(CURDATE(),"+str(i)+")"
         cursor.execute(sql)
@@ -49,17 +54,20 @@ for i in range (12,16): #poner el rango de dias desde hoy para actualizar
         #print ('Nreg=',nreg)
         
         if nreg==0:
-            try:
-                cursor.execute("""INSERT INTO diario (Fecha,maxVbat,minVbat,avgVbat,maxSOC,minSOC,
-                    avgSOC,maxIbat,minIbat,avgIbat,maxIplaca,avgIplaca,kWh_placa,kWhp_bat,kWhn_bat,
-                    kWh_consumo,maxTemp,minTemp,avgTemp)
-                    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", 
-                    (var[0][0],round(var[0][1],2),round(var[0][2],2),round(var[0][3],2),
-                     round(var[0][4],2),round(var[0][5],2),round(var[0][6],2),round(var[0][7],2),
-                     round(var[0][8],2),round(var[0][9],2),round(var[0][10],2),round(var[0][11],2),
-                     round(var[0][12],2),round(var[0][13],2),round(var[0][14],2),round(var[0][15],2),
-                     round(var[0][16],2),round(var[0][17],2),round(var[0][18],2)))
-
+            
+            try: # 26 campos
+                cursor.execute("""INSERT INTO diario (Fecha,maxVbat,minVbat,avgVbat,maxSOC,minSOC,avgSOC,maxIbat,minIbat,
+                avgIbat,maxIplaca,avgIplaca,Wh_placa,Whp_bat,Whn_bat,Wh_consumo,maxTemp,minTemp,avgTemp,
+                Whp_red,Whn_red,maxWred,minWred,avgWred,maxVred,minVred) 
+                VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", 
+                (var[0][0]         ,round(var[0][1],2) ,round(var[0][2],2) ,round(var[0][3],2) ,round(var[0][4],2) ,
+                round(var[0][5],2) ,round(var[0][6],2) ,round(var[0][7],2) ,round(var[0][8],2) ,round(var[0][9],2) ,
+                round(var[0][10],2),round(var[0][11],2),round(var[0][12],2),round(var[0][13],2),round(var[0][14],2),
+                round(var[0][15],2),round(var[0][16],2),round(var[0][17],2),round(var[0][18],2),round(var[0][19],2),
+                round(var[0][20],2),round(var[0][21],2),round(var[0][22],2),round(var[0][23],2),round(var[0][24],2),
+                round(var[0][25],2) ))
+            
+            
                 db.commit()    
                 log='Registro diario de hace '+str(i)+ ' dias creado'
                 logBD()
@@ -70,16 +78,19 @@ for i in range (12,16): #poner el rango de dias desde hoy para actualizar
                 logBD()
         else:
             try:
-                cursor.execute("""UPDATE diario SET maxVbat=%s,minVbat=%s,avgVbat=%s,maxSOC=%s,
-                    minSOC=%s,avgSOC=%s,maxIbat=%s,minIbat=%s,avgIbat=%s,maxIplaca=%s,avgIplaca=%s,
-                    kWh_placa=%s,kWhp_bat=%s,kWhn_bat=%s,kWh_consumo=%s,maxTemp=%s,minTemp=%s,
-                    avgTemp=%s
-                    WHERE Fecha=%s""", 
-                    (round(var[0][1],2),round(var[0][2],2),round(var[0][3],2),round(var[0][4],2),
-                    round(var[0][5],2),round(var[0][6],2),round(var[0][7],2),round(var[0][8],2),
-                    round(var[0][9],2),round(var[0][10],2),round(var[0][11],2),round(var[0][12],2),
-                    round(var[0][13],2),round(var[0][14],2),round(var[0][15],2),round(var[0][16],2),
-                    round(var[0][17],2),round(var[0][18],2),var[0][0]))
+                cursor.execute("""UPDATE diario SET
+                maxVbat=%s,minVbat=%s,avgVbat=%s  ,maxSOC=%s   , minSOC=%s  ,avgSOC=%s ,maxIbat=%s,
+                minIbat=%s,avgIbat=%s,maxIplaca=%s,avgIplaca=%s, Wh_placa=%s,Whp_bat=%s,
+                Whn_bat=%s,Wh_consumo=%s,maxTemp=%s,minTemp=%s,avgTemp=%s,Whp_red=%s,
+                Whn_red=%s,maxWred=%s,minWred=%s,avgWred=%s,maxVred=%s,minVred=%s
+                WHERE Fecha=%s""", 
+                (round(var[0][1],2),round(var[0][2],2) ,round(var[0][3],2) ,round(var[0][4],2) ,
+                round(var[0][5],2) ,round(var[0][6],2) ,round(var[0][7],2) ,round(var[0][8],2) ,
+                round(var[0][9],2) ,round(var[0][10],2),round(var[0][11],2),round(var[0][12],2),
+                round(var[0][13],2),round(var[0][14],2),round(var[0][15],2),round(var[0][16],2),
+                round(var[0][17],2),round(var[0][18],2),round(var[0][19],2),round(var[0][20],2),
+                round(var[0][21],2),round(var[0][22],2),round(var[0][23],2),round(var[0][24],2),
+                round(var[0][25],2),var[0][0]))
 
                 db.commit()
                 log='Registro diario de hace '+str(i)+ ' dias actualizado'
