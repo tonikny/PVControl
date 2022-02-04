b36852a463c552d671fc9881175d0c0981319e92
mleon55555 <mleon55555@gmail.com>
2022-02-04 10:17:02 +0100

    Inclusión string 2

--- huawei.py
+++ huawei.py
@@ -40,7 +40,7 @@ time.sleep(1)
 
 def leer_datos():
         
-    INV = hua.read_holding_registers(32016, 4)               # V e I de strings
+    INV = hua.read_holding_registers(32016, 8)               # V e I de strings
     VGR = hua.read_holding_registers(32066,1 )               # V red
     PP =  hua.read_holding_registers(32080, 2)               # Potencia campo FV    
     E  =  hua.read_holding_registers(32086,1 )               # Eficiencia DC/AC
@@ -61,22 +61,26 @@ def escribir_datos():
     if i != None:
         
         #Wred = i[11]
-        if i[11] >32768:  i[11] = i[11] - 65536           
+        if i[15] >32768:  i[15] = i[15] - 65536           
         
         datos ={}
         
         datos['Tiempo'] = time.strftime("%Y-%m-%d %H:%M:%S") 
         datos['Tiempo_sg'] = time.time()
-        datos['Wred'] = i[11]
-        datos['Wplaca'] = i[6]
-        datos['Vplaca'] = round(i[0]*0.1,2)
-        datos['Aux1'] = i[9]*0.001
-        datos['Temp'] = round(i[8]*0.1,2)
-        datos['EFF'] = round(i[7]*0.01,2)
+        datos['Wred'] = i[15]
+        datos['Wplaca'] = i[10]
+        datos['Vplaca_1'] = round(i[0]*0.1,2)
+        datos['Vplaca_2'] = round(i[2]*0.1,2)
+        datos['Vplaca'] = (datos['Vplaca_1'] + datos['Vplaca_2'])/2
+        datos['Iplaca_1'] = round(i[1]*0.01,2)
+        datos['Iplaca_2'] = round(i[3]*0.01,2)
+        datos['Aux1'] = i[13]*0.001
+        datos['Temp'] = round(i[12]*0.1,2)
+        datos['EFF'] = round(i[11]*0.01,2)
         
         datos['Consumo'] = round(datos['Wplaca']- datos['Wred'],2)
         
-        datos['Vred'] = round(i[4]*0.1,2)
+        datos['Vred'] = round(i[8]*0.1,2)
         if datos['Vred'] == 0: datos['Vred'] = 230
         
         datos['Ired'] = round(datos['Wred'] / datos['Vred'],2)
