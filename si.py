4b0e8b49081ae0439c0d7f449c618ee1b4413232
tonikny <dalecanya@gmail.com>
2022-02-12 22:13:10 +0100

    master -> desarrollo

--- si.py
+++ si.py
@@ -24,7 +24,7 @@ si.unit_id(3)
 si.open()
 
 Equipo = sys.argv[1]
-
+cont = 0
 crear_pkl = 0 # poner a 1 para versiones antiguas de PVControl que no usan BD en RAM
 
 DEBUG = False
@@ -98,22 +98,76 @@ def leer_datos():
 		print('No hay datos')
 		return(None)
 		
+def leer_datos_est():
+	
+	try:
+		t_to_abs = si.read_holding_registers(31007, 2)
+		t_to_abs = t_to_abs[1]
+		Temp = si.read_holding_registers(30849, 2)
+		Temp = Temp[1]*0.1                
+		v_abs = si.read_holding_registers(40085, 2)
+		v_abs = v_abs[1]*0.01 * 24 
+		v_flot = si.read_holding_registers(40091, 2)
+		v_flot = v_flot[1]*0.01 * 24
+		SOC_si = si.read_holding_registers(30845, 2)
+		SOC_si = SOC_si[1]
+	except:
+		print ('Error lectura V Objetivo')
+		logBD('Error de Lectura SI Temp,SOC...')
+		Vobj = Vobj + 0.5
+		si.close()
+		time.sleep(3)
+		si.open()
+		pass
+                
+	if t_to_abs == 0:
+		Vobj = v_flot - (Temp-20) * 24 * 0.004 - 0.8
+	if t_to_abs != 0:
+		Vobj = v_abs - (Temp-20) * 24 * 0.004 - 0.8
+		    
+	if Vobj < 53:
+		Vobj = 65.00
+		    
+	if DEBUG: 
+		print ('   Vabs', v_abs,'V_flot',v_flot, 't_to_abs', t_to_abs,'Vobj',Vobj,'SOC',SOC_si)
+		
+	try:
+		db = MySQLdb.connect(host = servidor, user = usuario, passwd = clave, db = basedatos)
+		cursor = db.cursor()
+		query = """ UPDATE parametros SET objetivo_PID = %s WHERE id_parametros =%s """
+		data = (Vobj,1)
+		cursor.execute(query,data)
+		db.commit()
+	except:	
+		
+		pass
+	try:
+		cursor.close()
+		
+	except:
+		pass  
+	return(None)
+		
 while True:
 	
 	try:
 	
 		datos = leer_datos()
+		if cont % 10 == 0:
+			leer_datos_est()
+			cont=1
+		cont+=1
 		
-		if DEBUG: print('datos=',datos)
+		if DEBUG: print('datos=',datos,'cont',cont)
 	
 		if datos != None:
 			if crear_pkl == 0:
-				try:####  ARCHIVOS RAM en BD ############
+				try:			####  ARCHIVOS RAM en BD ############
 					tiempo = time.strftime("%Y-%m-%d %H:%M:%S")
 			
 					salida = json.dumps(datos)
 					#print(salida)
-					sql = (f"UPDATE equipos SET tiempo = '{tiempo}',sensores = '{salida}' WHERE id_equipo = '{sys.argv[1]}' ") # grabacion en BD RAM
+					sql = (f"UPDATE equipos SET tiempo = '{tiempo}',sensores = '{salida}' WHERE id_equipo = '{sys.argv[1]}'") # grabacion en BD RAM
 					#print(sql)	
 					cursor.execute(sql)
 					db.commit()
