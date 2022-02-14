bcd08ed261931fe5b8b45e3021c768eaf08ea032
mleon55555 <mleon55555@gmail.com>
2022-02-14 23:38:50 +0100

    Se añaden campos a la grafica historica (1 dia) Es necesario actualizar el archivo Parametros_Web.js con el archivo Parametros_Web_DIST.js

--- html/Parametros_Web_DIST.js
+++ html/Parametros_Web_DIST.js
@@ -1,31 +1,18 @@
 
-// Pagina inicio_XXXX.php
-// Actualizar los relojes necesarios y demas variables de acuerdo a la instalacion
-// 
+// Pagina inicio.php
 
 	// Reloj Vbat
-	Vbat_min = 44; 
-	Vbat_bajo_amarillo = 48;
-	Vbat_verde = 50.8;
-	Vbat_alto_amarillo = 56;
-	Vbat_alto_rojo = 60;
-	Vbat_max = 64; 
-
-	// Reloj Vred    
-	Vred_min = 200; 
-	Vred_bajo_amarillo = 210;
-	Vred_verde = 215;
-	Vred_alto_amarillo = 240;
-	Vred_alto_rojo = 250;
-	Vred_max = 270; 
+	Vbat_min = 22; 
+	Vbat_bajo_amarillo = 24;
+	Vbat_verde = 25.4;
+	Vbat_alto_amarillo = 28;
+	Vbat_alto_rojo = 30;
+	Vbat_max = 32; 
 
 	// SOC
 	SOC_min=60;
 	SOC_max=100;
 
-	// AC/DC EFICIENCIA
-	EFF_min = 60;
-	EFF_max = 100;
 
 	// reloj Temp
 	Temp_bat_min = -10;
@@ -46,23 +33,16 @@
 	Consumo_watios_max = 5000;
 
 	Consumo_amperios_min= 0;
-	Consumo_amperios_amarillo = 60;
-	Consumo_amperios_rojo = 80;
-	Consumo_amperios_max= 100;
+	Consumo_amperios_amarillo = 125;
+	Consumo_amperios_rojo = 166;
+	Consumo_amperios_max= 210;
 
 	// Reloj Ibat/Iplaca
-	Intensidad_min = -65;
-	Intensidad_descarga_amarillo = -20;
-	Intensidad_carga_rojo = 40;
-	Intensidad_max = 65;
+	Intensidad_min = -130;
+	Intensidad_descarga_amarillo = -40;
+	Intensidad_carga_rojo = 80;
+	Intensidad_max = 130;
 
-	// Reloj Excedentes
-	Wred_min = -5000;
-	Wred_negativo_rojo = -2000;
-	Wred_negativo_amarillo = -1000;
-	Wred_positivo_amarillo = 500;
-	Wred_positivo_rojo = 2000;
-	Wred_max = 5000;
 
 	// Reloj Watios Placa
 	Watios_placa_baja_rojo = 300;
@@ -84,23 +64,18 @@
 
 	Vcelda_franja_min = 1.8;
 	Vcelda_franja_max = 2.5;
-
+	
 	// Grafico Tiempo Real
-	Escala_intensidad_min = -65;
-	Escala_intensidad_max = 100;
+	Escala_intensidad_min = -140;
+	Escala_intensidad_max = 240;
 
-	Escala_Vbat_min = 44;
-	Escala_Vbat_max = 140;
-	Vabs = 57.6;  // linea Vabs
-	Vflot = 54.4; // Linea Vflot
+	Escala_Vbat_min = 22;
+	Escala_Vbat_max = 35;
 
-	Escala_Wred_min = -5000;
-	Escala_Wred_max = 5000;
+	Vabs = 28.8;  // linea Vabs
+	Vflot = 27.2; // Linea Vflot
 
-	Vred_alto = 240;  // linea Vred alto
-	Vred_bajo = 200; // Linea Vred bajo
-
-	Escala_PWM_max = 400; 
+	Escala_PWM_max = 800; 
 	Escala_Vplaca_max = 400; 
 
 
@@ -111,55 +86,105 @@
 	SOC_min_rojo = 60;
 	SOC_min_naranja = 75;
 
-	Vbat_max_alta_rojo = 60;
-	Vbat_max_alta_naranja = 58;
-	Vbat_max_baja_rojo = 50;
-	Vbat_max_baja_naranja = 54;
+	Vbat_max_alta_rojo = 30;
+	Vbat_max_alta_naranja = 29;
+	Vbat_max_baja_rojo = 25;
+	Vbat_max_baja_naranja = 27;
 
-	Vbat_min_rojo = 44;
-	Vbat_min_naranja = 46;
-
-	EFF_max_rojo = 70;
-	EFF_max_naranja = 80;
-
-	EFF_min_rojo = 60;
-	EFF_min_naranja = 75;
-
-	Vred_max_alta_rojo = 245;
-	Vred_max_alta_naranja = 240;
-	Vred_max_baja_rojo = 205;
-	Vred_max_baja_naranja = 215;
-
-	Vred_min_rojo = 205;
-	Vred_min_naranja = 215;
-
-	Nombre_Aux1 = 'Aux1';
-	Unidades_Aux1 = 'V';
-
-	Nombre_Aux2 = 'Aux2';
-	Unidades_Aux2 = 'V';
-
-	Nombre_Aux3 = 'Aux3';
-	Unidades_Aux3 = 'V';
-
-	Nombre_Aux4 = 'Aux4';
-	Unidades_Aux4 = 'V';
-
-	Nombre_Aux5 = 'Aux5';
-	Unidades_Aux5 = 'V';
-
-	Nombre_Aux6 = 'Aux6';
-	Unidades_Aux6 = 'V';
-
-	Nombre_Aux7 = 'Aux7';
-	Unidades_Aux7 = 'V';
-
+	Vbat_min_rojo = 22;
+	Vbat_min_naranja = 23;
+	
+    Nombre_Aux1 = 'Vbat2_12v'
+    Unidades_Aux1 = 'V'
+    
+    Nombre_Aux2 = 'Vbat2_0v'
+    Unidades_Aux2 = 'V'
+    
+    Nombre_Aux3 = 'Wpl_H'
+    Unidades_Aux3 = 'W'
+    
+    Nombre_Aux4 = 'PACW_H'
+	Unidades_Aux4 = 'W'
+    
+    Nombre_Aux5 = 'COTEX'
+    Unidades_Aux5 = 'W'
+    
+    Nombre_Aux6 = 'Aux6'
+	Unidades_Aux6 = ''
+    
+    Nombre_Aux7 = 'Dif_Cel'
+	Unidades_Aux7 = 'mV'
+    
 // Pagina Historico1.php,  Historico_horas.php, historico_con_temp.php  ........
 
-// Se usan los mismos datos puestos en el Grafico Tiempo Real de inicio.php
-	Escala_Aux1_min = 0;
-	Escala_Aux1_max = 120;
-
-
-
-
+	// Se usan los mismos datos puestos en inicio.php mas los siguientes:
+	
+    Vred_max = 300
+    Watios_red_min = -2000
+    Watios_red_max = 2000
+    
+    Kwh_placa_max = 35
+    
+    Kwh_bat_min = -10
+    Kwh_bat_max = 10
+    
+    Kwh_red_min = -30
+    Kwh_red_max = 30
+    
+    Kwh_consumo_min = -30
+    Kwh_consumo_max = 30
+    
+    Aux1_min = 12
+	Aux1_max = 15
+    
+    Aux2_min = 12
+	Aux2_max = 15
+    
+    
+    // Poner a true si se quiere que aparezcan los ejes en la grafica o false para que no aparezcan
+    Eje_Intensidad = true // Ibat e Iplaca
+    Eje_Vbat = false
+    Eje_SOC = false
+    Eje_PWM = false
+    
+    Eje_Vplaca = false
+    Eje_Wplaca = false
+    Eje_Wred = false
+    Eje_Vred = false
+    Eje_Wconsumo = false
+    
+    Eje_Kwh_placa = false
+    Eje_Kwh_bat = false
+    Eje_Kwh_red = false
+    Eje_Kwh_consumo = false
+    
+    Eje_Temp = false
+    
+    Eje_Aux1 = false
+    Eje_Aux2 = false
+    
+    // Poner a true si se quiere que aparezca la grafica por defecto o false para que este desactivada
+    Ibat_visible = true
+    Iplaca_visible = true
+    Vbat_visible = true
+    SOC_visible = true
+    PWM_visible = true
+    
+    Vplaca_visible = false
+    Wplaca_visible = false
+    Wred_visible = false
+    Wconsumo_visible = false
+    Vred_visible = false
+    
+    Kwh_placa_visible = false
+    Kwh_bat_visible = false
+    Kwh_red_visible = false
+    Kwh_consumo_visible = false
+    
+    Temp_visible = false
+    
+    Modo_visible = true
+    
+    Aux1_visible = false
+    Aux2_visible = false
+    
--- html/historico1.php
+++ html/historico1.php
@@ -5,16 +5,13 @@ include ("cabecera.inc");
 require('conexion.php');
 
 //Coger datos grafica historico general
-$sql = "SELECT UNIX_TIMESTAMP(Tiempo)*1000 as Tiempo, SOC as SOCavg, Ibat as Ibatavg, Iplaca as Iplacaavg, Vbat as Vbatavg, Aux1 as Aux1avg, Vplaca as Vplacaavg, PWM as PWMavg
+$sql = "SELECT UNIX_TIMESTAMP(Tiempo)*1000 as Tiempo, SOC, Ibat, Iplaca, Vbat, Vplaca, PWM, Wplaca,Vred, Wred, Temp,
+          Wplaca - Vbat*Ibat - Wred as Wconsumo,
+          Wh_placa/1000 as Kwh_placa, (Whp_bat-Whn_bat)/1000 as Kwh_bat,(Whp_red-Whn_red)/1000 as Kwh_red,
+          (Wh_placa - Whp_bat + Whn_bat - Whp_red + Whn_red)/1000 as Kwh_consumo,
+          Mod_bat * 10 as Modo, Aux1, Aux2
         FROM datos WHERE Tiempo >= (NOW()- INTERVAL 25 HOUR)
         ORDER BY Tiempo";
-
-//$sql = "SELECT Tiempo, SOC as SOCavg, Ibat as Ibatavg, Iplaca as Iplacaavg, Vbat as Vbatavg, Vflot*10 as Vflotavg, Vplaca as Vplacaavg
-//        FROM datos WHERE DATE(Tiempo) >= SUBDATE(NOW(), INTERVAL 3 DAY)
-//        ORDER BY Tiempo";
-//$sql = "SELECT Tiempo, AVG(SOC) as SOCavg, AVG(Ibat) as Ibatavg, AVG(Iplaca) as Iplacaavg, AVG(Vbat) as Vbatavg, AVG(Vflot)*10 as Vflotavg
-//        FROM datos WHERE DATE(Tiempo) >= SUBDATE(NOW(), INTERVAL 7 DAY)
-//        GROUP BY DAY(Tiempo),((60/1)*HOUR(TIME(Tiempo))+FLOOR(MINUTE(TIME(Tiempo))/1)) ORDER BY Tiempo";
 
 if($result = mysqli_query($link, $sql)){
 
@@ -88,17 +85,18 @@ $(function ()
       panning: true,
       panKey: 'shift'
       },
-    title: {
-      text: 'SOC, Iplaca/Ibat - Vbat/Vplaca - PWM/Aux1 --- 1 DIA'
-      },
-    subtitle: {
-      //text: 'Permite Zoom XY'
-      },
+    //title: {
+    //  text: 'Grafica Diaria -- 1 DIA'
+    //  },
+    //subtitle: {
+    //  text: 'Permite Zoom XY'
+    //  },
     credits: {
       enabled: false
       },
     yAxis: [
-     {// ########## Valores eje Intensidad ######################
+     {// ########## 0 - Valores eje Intensidad ######################
+      visible: Eje_Intensidad,
       opposite: false,
       min: Escala_intensidad_min,
       max: Escala_intensidad_max,
@@ -112,7 +110,7 @@ $(function ()
       title: {
         align: 'high',
         offset: 0,
-        text: 'Ibat',//null
+        text: 'Ibat',
         rotation: 0,
         y: -5
         },
@@ -124,8 +122,8 @@ $(function ()
         }]
      },
      
-     
-     {// ########## Valores eje Vbat ######################
+     {// ########## 1 - Valores eje Vbat ######################
+      visible: Eje_Vbat,
       opposite: false,
       min: Escala_Vbat_min,
       max: Escala_Vbat_max,
@@ -139,7 +137,7 @@ $(function ()
       title: {
         align: 'high',
         offset: 0,
-        text: 'Vbat',//null
+        text: 'Vbat',
         rotation: 0,
         y: -10
         },
@@ -165,7 +163,8 @@ $(function ()
        }]
      },
      
-     {// ########## Valores eje SOC ######################
+     {// ########## 2 - Valores eje SOC ######################
+      visible: Eje_SOC,
       opposite: true,
       min: 20,
       max: 100 ,
@@ -179,7 +178,7 @@ $(function ()
       title: {
         align: 'high',
         offset: 0,
-        text: 'SOC',//null
+        text: 'SOC',
         rotation: 0,
         y: -10
         },
@@ -202,7 +201,8 @@ $(function ()
        }]
       },
       
-     {// ########## Valores eje PWM ######################
+     {// ########## 3 - Valores eje PWM ######################
+      visible: Eje_PWM,
       opposite: true,
       min: 0,
       max: Escala_PWM_max,
@@ -215,14 +215,15 @@ $(function ()
       title: {
         align: 'high',
         offset: 0,
-        text: 'PWM',//null
+        text: 'PWM',
         rotation: 0,
         y: -10
         },
                   
      },
       
-     {// ########## Valores eje Vplaca ######################
+     {// ########## 4 - Valores eje Vplaca ######################
+      visible: Eje_Vplaca,
       opposite: false,
       min: 0,
       max: Escala_Vplaca_max,
@@ -232,28 +233,226 @@ $(function ()
       title: {
         align: 'high',
         offset: 0,
-        text: 'Vplaca',//null
+        text: 'Vplaca',
         rotation: 0,
         y: -5
         },
       },
-      
-     {// ########## Valores eje Aux1 ######################
+   
+     {// ########## 5 - Valores eje Wplaca ######################
+      visible: Eje_Wplaca,
       opposite: true,
-      min: Escala_Aux1_min,
-      max: Escala_Aux1_max,
-      tickInterval: 5,
+      min: 0,
+      max: Watios_placa_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Wplaca',
+        rotation: 0,
+        y: -10
+        },
+      },
+            
+     {// ########## 6 - Valores eje Wred ######################
+      visible: Eje_Wred,
+      opposite: true,
+      min: Watios_red_min,
+      max: Watios_red_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Wplaca',
+        rotation: 0,
+        y: -10
+        },
+      },
+                
+     {// ########## 7 - Valores eje Wconsumo ######################
+      visible: Eje_Wconsumo,
+      opposite: true,
+      min: Consumo_watios_min,
+      max: Consumo_watios_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Wconsumo',
+        rotation: 0,
+        y: -10
+        },
+      },
+     
+     {// ########## 8 - Valores eje Vred ######################
+      visible: Eje_Vred,
+      opposite: true,
+      min: 0,
+      max: Vred_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Vred',
+        rotation: 0,
+        y: -10
+        },
+      },
+          
+     {// ########## 9 - Valores eje Kwh_placa ######################
+      visible: Eje_Kwh_placa,
+      opposite: true,
+      min: 0,
+      max: Kwh_placa_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Kwh_placa',
+        rotation: 0,
+        y: -10
+        },
+      },
+  
+     {// ########## 10 - Valores eje Kwh_bat ######################
+      visible: Eje_Kwh_bat,
+      opposite: true,
+      min: Kwh_bat_min,
+      max: Kwh_bat_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Kwh_bat',
+        rotation: 0,
+        y: -10
+        },
+      },
+            
+     {// ########## 11 - Valores eje Kwh_red ######################
+      visible: Eje_Kwh_red,
+      opposite: true,
+      min: Kwh_red_min,
+      max: Kwh_red_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Kwh_red',
+        rotation: 0,
+        y: -10
+        },
+      },
+   
+     {// ########## 12 - Valores eje Kwh_consumo ######################
+      visible: Eje_Kwh_consumo,
+      opposite: true,
+      min: Kwh_consumo_min,
+      max: Kwh_consumo_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Kwh_consumo',
+        rotation: 0,
+        y: -10
+        },
+      },
+     
+     {// ########## 13 - Valores eje Temp ######################
+      visible: Eje_Temp,
+      opposite: true,
+      min: Temp_bat_min,
+      max: Temp_bat_max,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      labels: {
+        //align: 'left',
+        y: 5
+        },
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Temp',
+        rotation: 0,
+        y: -10
+        },
+      },
+   
+     {// ########## 14 - Valores eje Aux1  ######################
+      visible: Eje_Aux1,
+      opposite: true,
+      min: Aux1_min,
+      max: Aux1_max,
+      tickInterval: 1,
       gridLineColor: 'transparent',
       minorGridLineColor: 'transparent',
       title: {
         align: 'high',
         offset: 0,
-        text: 'Aux1',//null
+        text: 'Aux1',
+        rotation: 0,
+        y: -5
+        },
+      },
+    
+     {// ########## 15 - Valores eje Aux2  ######################
+      visible: Eje_Aux2,
+      opposite: true,
+      min: Aux2_min,
+      max: Aux2_max,
+      tickInterval: 1,
+      gridLineColor: 'transparent',
+      minorGridLineColor: 'transparent',
+      title: {
+        align: 'high',
+        offset: 0,
+        text: 'Aux2',
         rotation: 0,
         y: -5
         },
       
-      }
+      },
       
      ],
     xAxis: {
@@ -293,40 +492,43 @@ $(function ()
     series: [
      {name: 'Ibat',
       type: 'spline',
+      visible: Ibat_visible,
       color: Highcharts.getOptions().colors[2],
       tooltip: {
         valueSuffix: ' A',
-        valueDecimals: 2,
+        valueDecimals: 1,
         },
       data: (function() {
         var data = [];
         <?php
           for($i = 0 ;$i<count($rawdata);$i++){
             ?>
-            data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Ibatavg"];?>]);
+            data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Ibat"];?>]);
             <?php } ?>
           return data;
         })()
      },
      {name: 'Iplaca',
       type: 'spline',
+      visible: Iplaca_visible,
       color: Highcharts.getOptions().colors[3],
       tooltip: {
         valueSuffix: ' A',
-        valueDecimals: 2,
+        valueDecimals: 1,
         },
       data: (function() {
         var data = [];
         <?php
           for($i = 0 ;$i<count($rawdata);$i++){
             ?>
-            data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Iplacaavg"];?>]);
+            data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Iplaca"];?>]);
             <?php } ?>
           return data;
         })()
      },
      {name: 'Vbat',
       type: 'spline',
+      visible: Vbat_visible,
       yAxis: 1,
       color: Highcharts.getOptions().colors[0],
       tooltip: {
@@ -338,13 +540,14 @@ $(function ()
         <?php
           for($i = 0 ;$i<count($rawdata);$i++){
             ?>
-            data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Vbatavg"];?>]);
+            data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Vbat"];?>]);
             <?php } ?>
           return data;
         })()
      },
      {name: 'SOC',
       type: 'spline',
+      visible: SOC_visible,
       yAxis: 2,
       color: Highcharts.getOptions().colors[1],
       tooltip: {
@@ -356,14 +559,14 @@ $(function ()
         <?php
           for($i = 0 ;$i<count($rawdata);$i++){
             ?>
-              data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["SOCavg"];?>]);
+              data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["SOC"];?>]);
               <?php } ?>
             return data;
         })()
      },
      {name: 'PWM',
-      visible: true,
       type: 'spline',
+      visible: PWM_visible,
       yAxis: 3,
       color: Highcharts.getOptions().colors[5],
       tooltip: {
@@ -375,17 +578,17 @@ $(function ()
         <?php
         for($i = 0 ;$i<count($rawdata);$i++){
           ?>
-          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["PWMavg"];?>]);
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["PWM"];?>]);
           <?php } ?>
         return data;
         })()  
         
      },
      {name: 'Vplaca',
-      visible: false,
       type: 'spline',
-      yAxis: 4, // poner 2 para escala del SOC
-      color: 'black', //Highcharts.getOptions().colors[20],
+      visible: Vplaca_visible,
+      yAxis: 4,
+      color: '#632D2D', //Highcharts.getOptions().colors[20],
       tooltip: {
         valueSuffix: ' V',
         valueDecimals: 0,
@@ -395,17 +598,197 @@ $(function ()
         <?php
         for($i = 0 ;$i<count($rawdata);$i++){
           ?>
-          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Vplacaavg"];?>]);
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Vplaca"];?>]);
           <?php } ?>
         return data;
         })()
-     }, 
-     
-     {name: 'Aux1',
-      visible: true,
+     },   
+     {name: 'Wplaca',
       type: 'spline',
+      visible: Wplaca_visible,
       yAxis: 5,
-      color: Highcharts.getOptions().colors[4],
+      color: '#E55FE5',
+      tooltip: {
+        valueSuffix: ' W',
+        valueDecimals: 0,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Wplaca"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },     
+     {name: 'Wred',
+      type: 'spline',
+      visible: Wred_visible,
+      yAxis: 6,
+      color: '#D882C9',
+      tooltip: {
+        valueSuffix: ' W',
+        valueDecimals: 0,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Wred"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },             
+     {name: 'Wconsumo',
+      type: 'spline',
+      visible: Wconsumo_visible,
+      yAxis: 7,
+      color: '#F39610',
+      tooltip: {
+        valueSuffix: ' W',
+        valueDecimals: 0,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Wconsumo"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },         
+     
+     {name: 'Vred',
+      type: 'spline',
+      visible: Vred_visible,
+      yAxis: 8,
+      color: '#C55FE5',
+      tooltip: {
+        valueSuffix: ' V',
+        valueDecimals: 0,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Vred"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },     
+
+     {name: 'Kwh_placa',
+      type: 'area',
+      visible: Kwh_placa_visible,
+      yAxis: 9,
+      fillOpacity: 0.2,
+      color: "#E55FE5",
+      tooltip: {
+        valueSuffix: ' Kwh',
+        valueDecimals: 1,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Kwh_placa"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },     
+     {name: 'Kwh_bat',
+      type: 'area',
+      visible: Kwh_bat_visible,
+      yAxis: 10,
+      fillOpacity: 0.2,
+      color: "#7C75D7",
+      tooltip: {
+        valueSuffix: ' Kwh',
+        valueDecimals: 1,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Kwh_bat"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },     
+     {name: 'Kwh_red',
+      type: 'area',
+      visible: Kwh_red_visible,
+      yAxis: 11,
+      fillOpacity: 0.2,
+      color: "#D882C9",
+      tooltip: {
+        valueSuffix: ' Kwh',
+        valueDecimals: 1,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Kwh_red"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },
+
+     {name: 'Kwh_consumo',
+      type: 'area',
+      visible: Kwh_consumo_visible,
+      yAxis: 12,
+      fillOpacity: 0.2,
+      color: "#F39610",
+      tooltip: {
+        valueSuffix: ' Kwh',
+        valueDecimals: 1,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Kwh_consumo"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },
+ 
+  
+     {name: 'Temp',
+      type: 'spline',
+      visible: Wplaca_visible,
+      yAxis: 13,
+      color: 'black',
+      tooltip: {
+        valueSuffix: ' ºC',
+        valueDecimals: 1,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Temp"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },
+     
+     {name: 'Modo',
+      type: 'spline',
+      visible: Modo_visible,
+      yAxis: 2,
+      color: '#1604FA',
       tooltip: {
         valueSuffix: ' ',
         valueDecimals: 0,
@@ -415,15 +798,53 @@ $(function ()
         <?php
         for($i = 0 ;$i<count($rawdata);$i++){
           ?>
-          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Aux1avg"];?>]);
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Modo"];?>]);
           <?php } ?>
         return data;
         })()
-      } 
-     ]
-
+      },
+       
+     {name: 'Aux1',
+      type: 'spline',
+      visible: Aux1_visible,
+      yAxis: 14,
+      color: Highcharts.getOptions().colors[6],
+      tooltip: {
+        valueSuffix: ' ',
+        valueDecimals: 2,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Aux1"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },     
+     {name: 'Aux2',
+      type: 'spline',
+      visible: Aux2_visible,
+      yAxis: 15,
+      color: Highcharts.getOptions().colors[8],
+      tooltip: {
+        valueSuffix: ' ',
+        valueDecimals: 2,
+        },
+      data: (function() {
+        var data = [];
+        <?php
+        for($i = 0 ;$i<count($rawdata);$i++){
+          ?>
+          data.push([<?php echo $rawdata[$i]["Tiempo"];?>,<?php echo $rawdata[$i]["Aux2"];?>]);
+          <?php } ?>
+        return data;
+        })()
+      },
+          
+    ]
     });
-
   });
 </script>
 
